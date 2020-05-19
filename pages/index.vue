659526e7f2b322ad675e9b870b5916db8c08cff6
jahidanowar <milon5143@gmail.com>
2020-05-19 23:58:01 +0530

    Index Updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,58 @@
 <template>
 
-   <v-container>
-     <div class="header">
-       <v-row align="center" justify="center">
-         <v-col>
-           <h1 class="header-title">We Provide Result Driven Digital Solution <span class="primary--text">.</span></h1>
-           <p class="header-description">We make websites for awesome business like yours. Let us help you to make your brand online . </p>
-           <v-btn color="primary" large tile depressed>Learn More</v-btn>
-           <v-btn color="primary" large outlined tile class="ml-3">Let's Talk</v-btn>
-         </v-col>
-         <v-col align="center">
-           <v-img src="https://foxflue.com/wp-content/uploads/2020/03/foxflue-web-design-illustration.png" max-width="400"></v-img>
-         </v-col>
-       </v-row>
-     </div>
-     <div class="content-area">
-       <div class="content-area-title text-center">
-         <hr class="divider">
-         <h2>What We Do ?</h2>
-       </div>
-     </div>
+  <div>
+  <v-container>
+    <div class="header">
+      <v-row align="center" justify="center">
+        <v-col>
+          <h1 class="header-title">We Provide Result Driven Digital Solution <span class="primary--text">.</span></h1>
+          <p class="header-description">We make websites for awesome business like yours. Let us help you to make your brand online . </p>
+          <v-btn color="primary" large tile depressed>Learn More</v-btn>
+          <v-btn color="primary" large outlined tile class="ml-3">Let's Talk</v-btn>
+        </v-col>
+        <v-col align="center">
+          <v-img src="https://foxflue.com/wp-content/uploads/2020/03/foxflue-web-design-illustration.png" max-width="400"></v-img>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
+  
+  <v-container fluid class="primary">
+    <v-container>
+      <v-row>
+        <v-col md="4">
+          <v-card flat >
+            <h2 class="countdown text-center">349 +</h2>
+            <v-card-text class="text-center">businesses have thier place on Internet</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col md="4">
+          <v-card flat >
+            <h2 class="countdown text-center">113 +</h2>
+            <v-card-text class="text-center">businesses have thier place on Internet</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col md="4">
+          <v-card flat >
+            <h2 class="countdown text-center">150 +</h2>
+            <v-card-text class="text-center">businesses have thier place on Internet</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
+  </v-container>
+
+  <v-container>
+    <div class="content-area">
+      <div class="content-area-title text-center">
+        <hr class="divider">
+        <h2>What We Do ?</h2>
+      </div>
+    </div>
+  </v-container>
+
+  </div>
+    
 
 </template>
 
@@ -60,12 +92,13 @@ export default {
   margin-bottom: 10px;
 }
 .header{
+  padding: 5% 0;
   &-title{
     font-size: 3.5rem;
     margin-bottom: 1rem;
   }
   &-description{
-    font-size: 1.2rem;
+    font-size: 1.4rem;
     margin-bottom: 1rem;
   }
 }
@@ -76,5 +109,11 @@ export default {
       font-size: 2rem;
     }
   }
+}
+.countdown{
+  text-align: center;
+  font-size: 3rem;
+  color: #f68c1c;
+  font-weight: bold;
 }
 </style>
